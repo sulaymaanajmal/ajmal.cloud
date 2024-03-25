ee3457a59c7b9d3af9d00f64dbe64025ecf2fcca
Sulaymaan Ajmal <102180599+sulaymaanajmal@users.noreply.github.com>
2024-03-25 19:33:51 +0000

    Update contact email

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -12,7 +12,7 @@ return (
         I&#39;m web developer and I love to build things.
       </p>
       <button className="bg-primary hover:bg-secondary font-bold py-2 px-4 m-5 rounded inline-flex items-center transform transition-transform ease duration-200 translate-y-0 hover:-translate-y-1 hover:shadow-lg">
-  <a href="mailto:sulaymaan@ajmal.cloud" className="mr-2">Say Hi!</a>
+  <a href="mailto:hello@ajmal.cloud" className="mr-2">Say Hi!</a>
   <i className='bx bxs-envelope text-xl'></i>
 </button>
     </main>
