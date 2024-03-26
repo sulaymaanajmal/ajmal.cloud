5c55f8373869bedec179b750f10eb3b2312472f2
Sulaymaan Ajmal <102180599+sulaymaanajmal@users.noreply.github.com>
2024-03-26 15:16:19 +0000

    Add vercel analytics

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const font = Space_Grotesk({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en" className="min-h-screen bg-background font-sans antialiased selection:bg-accent">
       <head>
       <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
+      <Analytics />
       </head>
       <body className={font.className}>{children}</body>
     </html>
